ee5cf68d88dd6ba518ad7428e650d9dbaf56ea73
george.zhenzhong <george.zhenzhong@carsales.com.au>
2012-05-15 17:54:04 +1000

    Add thinlet reference

--- PizzaVendorMachine/src/states/SelectPizzaState.java
+++ PizzaVendorMachine/src/states/SelectPizzaState.java
@@ -2,10 +2,18 @@ package states;
 import java.io.IOException;
 
 import rmit.utils.workflow.State;
+import thinlet.*;
 
 public class SelectPizzaState extends State {
 	@Override
 	public void start() {
+		Thinlet thinlet = new Thinlet();
+		try {
+			thinlet.add(thinlet.parse("/UIDefinition/SelectPizza.xml"));
+			new FrameLauncher("Select Pizza", thinlet, 320, 240);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
